ce68735b64ab24198fd79ad2f4654addd7de0938
Tanner Heffner <tannerjheffner@gmail.com>
2015-04-24 10:54:14 -0700

    add initial files from processing port

---
+++ README.md
@@ -0,0 +1,17 @@
+# Star Music
+## by Tanner Heffner
+### Date: April 24th, 2015
+#### Description
+
+John Cage wrote a few sonatas from the placement of stars in the sky.
+He used sections of starcharts to transpose the locations of certain stars to their equivalent on
+a music staff. My idea for this project is similar--- taking a random image of stars to play a note
+corresponding to the individual locations of stars on screen.
+
+#### Setup instructions
+1. Clone this git repository
+2. Open index.html in your browser
+
+#### Copyright © 2015, Tanner Heffner
+
+#### License: [MIT](https://github.com/twbs/bootstrap/blob/master/LICENSE)
---
+++ sketch.js
@@ -0,0 +1,64 @@
+var m0 //, m1, m2, m3, m4, m5;
+
+var maps = [m0];
+
+
+function preload() {
+  m0 = loadImage("img/map1.jpg");
+  // m1 = loadImage("img/map2.gif");
+  // m2 = loadImage("img/map3.gif");
+  // m3 = loadImage("img/map4.jpg");
+  // m4 = loadImage("img/map5.jpg");
+  // m5 = loadImage("img/map6.jpg");
+
+//  var maps = [m0, m1, m2, m3, m4, m5]
+
+  imageMode(CENTER);
+
+  var randMap = int(random(maps.length));
+  image(maps[randMap], width/2, height/2);
+
+}
+
+function setup(){
+  createCanvas(600, 600);
+  background(0);
+//  minim = new Minim(this);
+//  var star = minim.getLineOut();
+//  star.setTempo(120f);
+}
+
+function draw(){
+}
+//
+// /* MOUSEPRESS PLAYS A NOTE BASED ON mouseX/mouseY COORDS + PIXEL RGB VALUES */
+// function mousePressed(){
+//   var S;
+//   var x;
+//   var y;
+//   var z;
+//   x = mouseX;
+//   y = mouseY;
+//   S = get(x, y);
+//   z = red(S)+green(S)+blue(S);
+//
+//  // this displays the RGB & alpha values for the pixel at (mouseX, mouseY) on any mousePress
+//   println("red: "+red(S)+" green "+green(S)+" blue "+blue(S)+" alpha "+alpha(S));
+//   println(mouseX, mouseY);
+//
+// //  adjust z <= for the brightness of pixels that will trigger sound, although if it's too low
+// //  any pixel will play a note, which defeats the purpose of selecting for stars only
+//   if (z <= 16) {
+//     star.mute();
+//   }
+//   else {
+//     star.unmute();
+//     star.playNote(x+y+z);
+//   }
+// }
+//
+/* ANY KEYPRESS CHANGES BG IMAGE */
+function keyPressed(){
+   var randMap = int(random(maps.length));
+   image(maps[randMap], width/2, height/2);
+}
